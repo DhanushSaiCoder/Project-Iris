060c74dd473a09d36d843e2f6eb32d69c6534b9e
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-22 19:24:55 +0530

    Refactor code structure for improved readability and maintainability

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import styles from "./Header.module.css"
+import logo from "../../assets/images/logo.png"
+import { Settings } from 'lucide-react'
+import { useNavigate } from 'react-router-dom';
+
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleSettingsClick = () => {
+        navigate('/settings');
+    }
+
     return (
         <div className={styles.Header}>
-            <h2>HEADER</h2>
+            <img className={styles.logoImg} src={logo} alt="My Brand Logo" />
+            <h2 className={styles.headerTxt}>PROJECT IRIS</h2>
+            <Settings className={styles.settingsIcon} onClick={handleSettingsClick}/>
         </div>
     );
 }

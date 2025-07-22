0cb66718dab61090967dd7adbc0cb00eee2b00ab
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-22 19:49:01 +0530

    Add VideoStream component and enhance HomePage layout with status and start button

---
+++ frontend/src/components/Home/VideoStream.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./VideoStream.module.css";
+
+const VideoStream = () => {
+    return (
+        <>
+            <div className={styles.VideoStream}></div>
+        </>
+    );
+};
+
+export default VideoStream;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import VideoStream from "../components/Home/VideoStream";
+import styles from "./HomePage.module.css";
 
 const HomePage = () => {
     return (
-        <div > 
-            <h1>HOME PAGE</h1>
+        <div>
+            <div className={styles.videoStreamDiv}>
+                <VideoStream />
+            </div>
+            <div className={styles.statusDiv}>
+                <p>
+                    <span className={styles.statusSpan}>Status:</span> Ready to
+                    detect obstacles.
+                </p>
+            </div>
+            <div className={styles.startBtnDiv}>
+                <button className={styles.startBtn}>Start Detection</button>
+            </div>
         </div>
     );
-}
+};
 
 export default HomePage;

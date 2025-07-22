dec05aacf764eea57e77943ae52b1d9db84f25ab
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-22 18:28:51 +0530

    Refactor Header component to use CSS modules for styling and update global styles in App.css

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from "./Header.module.css"
 
 const Header = () => {
     return (
-        <div>
+        <div className={styles.Header}>
             <h2>HEADER</h2>
         </div>
     );

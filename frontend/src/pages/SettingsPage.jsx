7997b845da647c71ec19191f2fe738ea2244694f
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-22 20:31:38 +0530

    Refactor SettingsPage component layout and styling for improved structure and user experience

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import styles from "./SettingsPage.module.css";
+import { ChevronLeft } from "lucide-react";
 
 const SettingsPage = () => {
     return (
-        <div>
-            <h1>SETTINGS PAGE</h1>
+        <div className={styles.Settings}>
+            <div className={styles.settingsHeader}>
+                <div className={styles.settingsBackDiv}>
+                    <ChevronLeft />
+                    Back
+                </div>
+                <h2>SETTINGS</h2>
+            </div>
         </div>
     );
-}
+};
 
 export default SettingsPage;

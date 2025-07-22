de17ef7d35198a57e038b07115fa7b21b20256c2
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-22 20:08:48 +0530

    Refactor Footer component to use CSS modules for styling and improve layout

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,8 +3,10 @@ import styles from "./Footer.module.css"
 
 const Footer = () => {
     return (
-        <div>
-            <h2>footer div</h2>
+        <div className={styles.Footer}>
+            <p><span className={styles.helpIcon}>?</span> Help</p>
+            <p><span className={styles.privacyNotesIcon}>!</span> Privacy Notes</p>
+            <p><span className={styles.developersIcon}>&lt;/&gt;</span> Developers</p>
         </div>
     );
 }

ab4219bd1f205bd71188f48514b6f10f7ebff22d
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-07-22 20:15:09 +0530

    Refactor Footer and Header components for improved navigation handling and code consistency

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,14 +1,36 @@
-import React from 'react';
-import styles from "./Footer.module.css"
+import React from "react";
+import styles from "./Footer.module.css";
+import { useNavigate } from "react-router-dom";
 
 const Footer = () => {
+    const navigate = useNavigate()
     return (
+
         <div className={styles.Footer}>
-            <p><span className={styles.helpIcon}>?</span> Help</p>
-            <p><span className={styles.privacyNotesIcon}>!</span> Privacy Notes</p>
-            <p><span className={styles.developersIcon}>&lt;/&gt;</span> Developers</p>
+            <p
+                onClick={() => {
+                    navigate("/help");
+                }}
+            >
+                <span className={styles.helpIcon}>?</span> Help
+            </p>
+            <p
+                onClick={() => {
+                    navigate("/privacy-notes");
+                }}
+            >
+                <span className={styles.privacyNotesIcon}>!</span> Privacy Notes
+            </p>
+            <p
+                onClick={() => {
+                    navigate("/developers");
+                }}
+            >
+                <span className={styles.developersIcon}>&lt;/&gt;</span>{" "}
+                Developers
+            </p>
         </div>
     );
-}
+};
 
 export default Footer;

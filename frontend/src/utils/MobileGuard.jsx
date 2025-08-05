54943e56c1ca6ebd2f6457fb862f7c7fac640b00
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-08-05 11:03:59 +0530

    -MobileGuard

--- frontend/src/utils/MobileGuard.jsx
+++ frontend/src/utils/MobileGuard.jsx
@@ -1,17 +1,7 @@
 // components/MobileGuard.jsx
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { isMobileDevice } from '../hooks/isMobile';
 
 export function MobileGuard({ children }) {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!isMobileDevice()) {
-      navigate('/admin');  // or whatever path you choose
-    }
-  }, [navigate]);
-
   // If it’s mobile, render the protected UI
-  return isMobileDevice() ? children : null;
+  return children;
 }

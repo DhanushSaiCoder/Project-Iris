3cbbe5d0f69c8a86504613c3bb127447c7181f99
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-08-07 22:25:26 +0530

    feat: Conditionally render Admin Dashboard button based on user role

--- frontend/src/pages/SettingsPage.jsx
+++ frontend/src/pages/SettingsPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { ChevronLeft } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from 'jwt-decode';
+import { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 import styles from "./SettingsPage.module.css";
 import AlertDistance from "../components/SettingsPage/AlertDistance/AlertDistance";
@@ -14,6 +17,23 @@ import LogoutButton from "../components/SettingsPage/LogoutButton/LogoutButton";
 
 const SettingsPage = () => {
     const navigate = useNavigate();
+    const { token } = useContext(AuthContext);
+
+    const getUserRoleFromToken = () => {
+        if (token) {
+            try {
+                const decodedToken = jwtDecode(token);
+                return decodedToken.role;
+            } catch (error) {
+                console.error("Error decoding token:", error);
+                return null;
+            }
+        }
+        return null;
+    };
+
+    const userRole = getUserRoleFromToken();
+    const isAdminOrDeveloper = userRole === 'admin' || userRole === 'developer';
     return (
         <div className={styles.Settings}>
             <div className={styles.settingsHeader}>
@@ -37,7 +57,7 @@ const SettingsPage = () => {
                 <DeveloperMode />
                 <div className={styles.buttonsDiv}>
                     <ReCaliberateDevice />
-                    <AdminDashboardBtn />
+                    {isAdminOrDeveloper && <AdminDashboardBtn />}
                 </div>
                 <LogoutButton />
             </div>
